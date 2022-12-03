98ecce8e595e0a4f14748b7a676fade963e08286
PaulJonard <paul.jonard@etu.univ-savoie.fr>
2022-12-03 21:39:06 +0100

    Sockets

--- Serveur Python/socket_server.py
+++ Serveur Python/socket_server.py
@@ -1,32 +1,27 @@
 import socket
 
+def donnerPersonneCompetente(listenSocket):
+    print("Le serveur est prêt à donner la personne. " + "Il est en attente de demandes de client(s) sur le port . \n" + str(listenSocket.getsockname()[0]) + str(listenSocket.getsockname()[1]))
+    try:
+        while True:
+            (clientsocket, address) = listenSocket.accept()
+            try:
+                messageDuServeur = clientsocket.recv(1024).decode()
+                clientsocket.send(messageDuServeur.encode())
+            finally:
+                clientsocket.close()
+    finally:
+        listenSocket.close()
 
-def server_program():
-    # get the hostname
-    host = socket.gethostname()
-    port = 4999  # initiate port no above 1024
-
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
-
-    conn.close()  # close the connection
 
 
 if __name__ == '__main__':
-    server_program()
+    serverSocket = socket.socket()
+    # Bind and listen
+    serverSocket.bind(("127.0.0.1",9090))
+    serverSocket.listen()
+    try:
+        #ouvrirConnexion(serverSocket)
+        donnerPersonneCompetente(serverSocket)
+    except Exception as e:
+        print(e)
